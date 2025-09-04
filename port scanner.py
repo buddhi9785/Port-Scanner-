c47e9586de6117310b0d41257a0d4af6b6d8729b
Buddhi Prakash Saini <buddhisaini@gmail.com>
2025-09-04 12:33:27 +0530

    port scanner.py

--- port scanner.py
+++ port scanner.py
@@ -1,3 +1,34 @@
+def print_banner():
+    banner = r"""
+                                                                                                                              
+                                                                                                                              
+                                 ___                                                                                          
+,-.----.                       ,--.'|_                                                                                        
+\    /  \    ,---.    __  ,-.  |  | :,'                                                  ,---,      ,---,             __  ,-. 
+|   :    |  '   ,'\ ,' ,'/ /|  :  : ' :           .--.--.                            ,-+-. /  | ,-+-. /  |          ,' ,'/ /| 
+|   | .\ : /   /   |'  | |' |.;__,'  /           /  /    '     ,---.     ,--.--.    ,--.'|'   |,--.'|'   |   ,---.  '  | |' | 
+.   : |: |.   ; ,. :|  |   ,'|  |   |           |  :  /`./    /     \   /       \  |   |  ,"' |   |  ,"' |  /     \ |  |   ,' 
+|   |  \ :'   | |: :'  :  /  :__,'| :           |  :  ;_     /    / '  .--.  .-. | |   | /  | |   | /  | | /    /  |'  :  /   
+|   : .  |'   | .; :|  | '     '  : |__          \  \    `. .    ' /    \__\/: . . |   | |  | |   | |  | |.    ' / ||  | '    
+:     |`-'|   :    |;  : |     |  | '.'|          `----.   \'   ; :__   ," .--.; | |   | |  |/|   | |  |/ '   ;   /|;  : |    
+:   : :    \   \  / |  , ;     ;  :    ;         /  /`--'  /'   | '.'| /  /  ,.  | |   | |--' |   | |--'  '   |  / ||  , ;    
+|   | :     `----'   ---'      |  ,   /         '--'.     / |   :    :;  :   .'   \|   |/     |   |/      |   :    | ---'     
+`---'.|                         ---`-'            `--'---'   \   \  / |  ,     .-./'---'      '---'        \   \  /           
+  `---`                                                       `----'   `--`---'                             `----'            
+                                                                                                                              
+    """
+    print(banner)
+
+    info = """
+    =[ Port Scanner v1.0 ]
+
+    """
+    print(info)
+
+
+if __name__ == "__main__":
+    print_banner() 
+
 import socket
 
 # Target IP address or hostname
@@ -11,12 +42,18 @@ print(f"\nScanning {target} from port {start_port} to {end_port}...\n")
 
 for port in range(start_port, end_port + 1):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5)  # 0.5 seconds timeout
+    s.settimeout(0.5)
 
     try:
         result = s.connect_ex((target, port))
         if result == 0:
-            print(f"✅ Port {port} is OPEN")
+            try:
+                service = socket.getservbyport(port, "tcp")
+            except:
+                service = "unknown"
+            print(f"✅ Port {port} ({service}) is OPEN")
+        else:
+            print(f"❌ Port {port} is CLOSED")
         s.close()
     except KeyboardInterrupt:
         print("\nScan stopped by user.")
